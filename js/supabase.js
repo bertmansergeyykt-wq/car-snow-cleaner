e172a9f3f1f04ef6fc18a9bf4c519949bf181fbf
bertmansergeyykt-wq <bertmansergeyykt@gmail.com>
2026-09-12 23:48:08 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,23 @@
+// ============================================================
+// SUPABASE
+// ============================================================
 
+export const SUPABASE_URL =
+  "https://dghcnwzqqmrsysewnvkn.supabase.co";
+
+export const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_OObcKHZ0AGc5AI5c_3KY-Q_GpzEKI8C";
+
+export const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+  );
+
+
+// ============================================================
+// EDGE FUNCTION
+// ============================================================
+
+export const QUICK_PROCESSOR_URL =
+  `${SUPABASE_URL}/functions/v1/quick-processor`;
