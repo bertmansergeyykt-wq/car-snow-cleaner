8abbe3ac5cee0b9cdeaaaddbd2d59ddb9e3809bb
bertmansergeyykt-wq <bertmansergeyykt@gmail.com>
2026-09-13 01:44:09 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -111,7 +111,12 @@ async function registerTelegramUser() {
           method: "POST",
 
           headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+
+            "Authorization":
+              `Bearer ${
+                window.Telegram?.WebApp?.initData || ""
+              }`
           },
 
           body: JSON.stringify({
